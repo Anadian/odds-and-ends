60951f16bd52b2c9458ae2085516b7b79641f878
Anadian <supercamman20@gmail.com>
2017-05-22 10:43:41 -0400

    Lets try that again

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-chmod 777 getrom searchrom
+chmod 777 getrom searchrom empty-dir git-lazy
 ln getrom /usr/local/bin/getrom
 ln searchrom /usr/local/bin/searchrom
+ln empty-dir /usr/local/bin/empty-dir
+ln git-lazy /usr/local/bin/git-lazy
 ln vimrc ~/.vimrc
 ln expansions ~/.expansions
