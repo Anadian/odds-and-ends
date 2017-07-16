2f803c6c85e1cdbfa4da2e3f3f8a5faea8bc4a28
Anadian <supercamman20@gmail.com>
2017-07-16 08:12:13 -0400

    Added clang-*s.

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-chmod 777 getrom searchrom empty-dir git-lazy git-current;
+chmod 777 getrom searchrom empty-dir git-lazy git-current clang-search-paths clang-linker-info clang-predefined-macros;
 ln getrom /usr/local/bin/getrom;
 ln searchrom /usr/local/bin/searchrom;
 ln empty-dir /usr/local/bin/empty-dir;
 ln git-lazy /usr/local/bin/git-lazy;
 ln git-current /usr/local/bin/git-current;
+ln clang-search-paths /usr/local/bin/clang-search-paths
+ln clang-linker-info /usr/local/bin/clang-linker-info
+ln clang-predefined-macros /usr/local/bin/clang-predefined-macros
 ln vimrc ~/.vimrc
 ln expansions ~/.expansions
