60f17027188aa72c6681b2e7cf1cfad76b24f66f
WillAnad <willanad@yandex.com>
2017-07-23 14:35:14 -0400

    Modified install.sh?

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ ln searchrom /usr/local/bin/searchrom;
 ln empty-dir /usr/local/bin/empty-dir;
 ln git-lazy /usr/local/bin/git-lazy;
 ln git-current /usr/local/bin/git-current;
-ln vimrc ~/.vimrc
-ln expansions ~/.expansions
+ln vimrc ~/.vimrc;
+ln expansions ~/.expansions;
