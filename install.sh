a2e355be0c67ba8bee1218bec4b174d2261d76ca
Anadian <supercamman20@gmail.com>
2017-08-28 15:46:30 -0400

    Added git-update.

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-chmod 777 getrom searchrom remove.pl empty-dir git-lazy git-current clang-search-paths clang-linker-info clang-predefined-macros gcc-predefined-macros gcc-search-paths;
+chmod 777 getrom searchrom remove.pl empty-dir git-lazy git-current git-update clang-search-paths clang-linker-info clang-predefined-macros gcc-predefined-macros gcc-search-paths;
 ln -f remove.pl /usr/local/bin/remove.pl;
 ln -f getrom /usr/local/bin/getrom;
 ln -f searchrom /usr/local/bin/searchrom;
 ln -f empty-dir /usr/local/bin/empty-dir;
 ln -f git-lazy /usr/local/bin/git-lazy;
 ln -f git-current /usr/local/bin/git-current;
+ln -f git-update /usr/local/bin/git-update;
 ln -f clang-search-paths /usr/local/bin/clang-search-paths;
 ln -f clang-linker-info /usr/local/bin/clang-linker-info;
 ln -f clang-predefined-macros /usr/local/bin/clang-predefined-macros;
