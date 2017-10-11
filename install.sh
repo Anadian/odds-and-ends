9e8a9e0bbe3042b8da8e76621f02f51eb0422cb4
Anadian <supercamman20@gmail.com>
2017-10-11 15:36:30 -0400

    Added full-path command a shell to prompt.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod 777 getrom searchrom remove.pl empty-dir git-lazy git-current git-update clang-search-paths clang-linker-info clang-predefined-macros gcc-predefined-macros gcc-search-paths crontab;
+chmod 777 getrom searchrom remove.pl empty-dir git-lazy git-current git-update clang-search-paths clang-linker-info clang-predefined-macros gcc-predefined-macros gcc-search-paths crontab full-path;
 ln -f remove.pl /usr/local/bin/remove.pl;
 ln -f getrom /usr/local/bin/getrom;
 ln -f searchrom /usr/local/bin/searchrom;
@@ -12,6 +12,7 @@ ln -f clang-linker-info /usr/local/bin/clang-linker-info;
 ln -f clang-predefined-macros /usr/local/bin/clang-predefined-macros;
 ln -f gcc-predefined-macros /usr/local/bin/gcc-predefined-macros;
 ln -f gcc-search-paths /usr/local/bin/gcc-search-paths;
+ln -f full-path /usr/local/bin/full-path;
 ln -f profilerc ~/.profilerc;
 ln -f vimrc ~/.vimrc;
 ln -f expansions ~/.expansions;
