850e544a4e68d34151a86d582925456482e99599
Anadian <willanad@yandex.com>
2019-02-24 00:00:18 -0500

    Cron.sh auto commit.

--- create-node-project-directories.sh
+++ create-node-project-directories.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-mkdir -p node/npm/node_modules;
-cd node/npm;
-npm install;
-cd ../..;
-ln -s node/npm/node_modules node_modules;
-ln node/npm/package-lock.json package-lock.json;
-mkdir -p scripts;
-mkdir -p source;
-mkdir -p Resources;
-echo "#$1" > README.md;
-git init;
-git remote add origin https://github.com/Anadian/$1.git;
-git add --all .;
-git commit -m 'First commit.';
-git push -u origin master;
+if [[ $1 == '' ]]; then
+	echo 'Error: project name not specified.';
+else
+	mkdir -p node/npm/node_modules;
+	cd node/npm;
+	npm install;
+	cd ../..;
+	ln -s node/npm/node_modules node_modules;
+	ln node/npm/package-lock.json package-lock.json;
+	mkdir -p scripts;
+	mkdir -p source;
+	mkdir -p Resources;
+	echo "#$1" > README.md;
+	git init;
+	git remote add origin https://github.com/Anadian/$1.git;
+	git add --all .;
+	git commit -m 'First commit.';
+	git push -u origin master;
+fi
