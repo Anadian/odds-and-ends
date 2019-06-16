eeedc330ca4480979f3c8674462ac146c6c875d4
Anadian <willanad@yandex.com>
2019-06-16 18:05:23 -0400

    Updated termux_init.sh

--- termux_init.sh
+++ termux_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkg install man linux-man-pages openssl bash coreutils clang make tar xz-utils p7zip nodejs perl lua openssh golang golang-doc grep curl wget -y;
+pkg install man linux-man-pages openssl openssl-tool bash coreutils clang make tar xz-utils p7zip nodejs perl lua openssh golang golang-doc grep curl wget -y;
 cp ./profilerc ~/.profilerc;
 git config --global user.email 'willanad@yandex.com';
 git config --global user.name 'Anadian';
