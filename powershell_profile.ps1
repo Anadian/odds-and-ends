4c0d9cc349f8c0d0314b8a2a37289b07b1fb1454
Anadian <willanad@yandex.com>
2019-09-19 22:58:29 -0400

    Updating powershell_profile.ps1

--- powershell_profile.ps1
+++ powershell_profile.ps1
@@ -5,3 +5,9 @@ New-Alias -Name 'which' -Value 'where.exe' -Description 'Makes where work correc
 #Function arg-test{ $args[0]; $args[1]; $args; $args[2..($args.Length - 1)];}
 Function ln{ New-Item -ItemType HardLink -Path $args[0] -Value $args[1] $args[2..($args.Length - 1)] }
 Function diff-plus{ Compare-Object -ReferenceObject $(Get-Content $args[0]) -DifferenceObject $(Get-Content $args[1]) -CaseSensitive -IncludeEqual }
+Set-Alias -Name 'wget' -Value 'C:\ProgramData\chocolatey\bin\wget.exe' -Description 'Use the real wget.' -Option AllScope
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
