c08f77c81d2cb3ec059b71a1230c338a866210cf
Anadian <willanad@yandex.com>
2019-11-10 23:48:07 -0500

    Additions to powershell_profile.ps1

--- powershell_profile.ps1
+++ powershell_profile.ps1
@@ -1,5 +1,10 @@
 Set-PSReadlineOption -BellStyle None
 $Env:PATH += ';C:\Windows\Microsoft.NET\Framework64\v4.0.30319;C:\Windows\Microsoft.NET\Framework\v4.0.30319;C:\Program Files\grepWin;C:\Program Files (x86)\Notepad++;C:\Program Files\Notepad++'
+$Env:PAGER = 'less'
+$Env:EDITOR = 'nvim'
+$Env:VISUAL = 'nvim'
+New-Alias -Name 'more' -Value 'less' -Description 'Use Unix-style less instead of MS-Style more.'
+New-Alias -Name 'vim' -Value 'nvim' -Description 'Uses NVIM.'
 Function choco-plus{ choco $args -dy }
 New-Alias -Name 'which' -Value 'where.exe' -Description 'Makes where work correctly.'
 #Function arg-test{ $args[0]; $args[1]; $args; $args[2..($args.Length - 1)];}
