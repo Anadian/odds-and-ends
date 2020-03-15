2b69c285bc4277e145ce4620cc4978403aabb4db
Anadian <willanad@yandex.com>
2020-03-15 00:35:38 -0400

    job.ps1 auto commit

--- job.ps1
+++ job.ps1
@@ -3,12 +3,13 @@
 Write-Output 'OddsandEnds\job.ps1' >> ~\scheduled_jobs.log;
 Get-Date -Format 'FileDateTimeUniversal' >> ~\scheduled_jobs.log;
 $script_directory = $PSScriptRoot;
-Write-Output $script_directory;
+Write-Output $script_directory; >> ~\scheduled_jobs.log;
 Set-Location -Path $script_directory;
-git add --all .;
-git commit -m 'job.ps1 auto commit';
+git add --all . >> ~\scheduled_jobs.log;
+git commit -m 'job.ps1 auto commit' >> ~\scheduled_jobs.log;
 if( $LastExitCode -eq 0 ){
-	git push origin master;
+	git push origin master >> ~\scheduled_jobs.log;
 } else{
-	git pull origin master;
+	git pull origin master >> ~\scheduled_jobs.log;
 }
+exit;
