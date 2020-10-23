99ae2f6df871b441eb2efb68180740923bc204a8
Anadian <willanad@yandex.com>
2020-10-23 00:00:06 -0400

    Cron.sh auto commit.

--- scripts/bash/gnome-screen-idle-delay-10.sh
+++ scripts/bash/gnome-screen-idle-delay-10.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "$(date --utc --iso-8601='seconds') Setting the screen to turn off after ten seconds of no input." >> ~/gnome-screen.log;
+echo "$(date --utc --iso-8601='seconds') Setting the screen to turn off after 10 seconds of no input." >> ~/gnome-screen.log;
 gsettings set org.gnome.desktop.session idle-delay 10;
