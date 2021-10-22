08bf6cb2256b62163ffd018f83995e84d8dad1e8
Anadian <willanad@yandex.com>
2021-10-22 00:00:07 -0400

    Cron.sh auto commit.

--- install.sh
+++ install.sh
@@ -3,12 +3,16 @@
 mode=0;
 if [[ $1 == 'uninstall' ]]; then
 	mode=1;
+	install_command='rm -f';
 elif [[ $1 == 'symbolic' ]]; then
 	mode=2;
+	install_command='ln -fs';
 elif [[ $1 == 'copy' ]]; then
 	mode=3;
+	install_command='cp -f';
 else
 	mode=0;
+	install_command='ln -f';
 fi
 if [[ $EUID == 0 ]]; then
 	#printf 'We are root.\n';
@@ -20,70 +24,51 @@ else
 	mkdir -p $install_directory;
 fi
 declare -A Map=(
-	['scripts/bash/clang-linker-info.sh']="$install_directory/clang-linker-info"
-);
-if [[ $mode != 1 ]]; then
 	#scripts/bash
-	chmod 777 'scripts/bash/clang-linker-info.sh';
-	ln -f 'scripts/bash/clang-linker-info.sh' $install_directory/clang-linker-info;
-	chmod 777 'scripts/bash/clang-predefined-macros.sh';
-	ln -f 'scripts/bash/clang-predefined-macros.sh' $install_directory/clang-predefined-macros;
-	chmod 777 'scripts/bash/clang-search-paths.sh';
-	ln -f 'scripts/bash/clang-search-paths.sh' $install_directory/clang-search-paths;
-	#chmod 777 'scripts/bash/clense-bad-roms.sh';
-	#ln -f 'scripts/bash/clense-bad-roms.sh' $install_directory/clense-bad-roms;
-	chmod 777 'scripts/bash/create-c-project-directories.sh';
-	ln -f 'scripts/bash/create-c-project-directories.sh' $install_directory/create-c-project-directories;
-	#chmod 777 'scripts/bash/create-node-project-directories.sh';
-	#ln -f 'scripts/bash/create-node-project-directories.sh' $install_directory/create-node-project-directories;
-	#chmod 777 'scripts/bash/create-node-project-directory.sh';
-	#ln -f 'scripts/bash/create-node-project-directory.sh' $install_directory/create-node-project-directory;
-	chmod 777 'scripts/bash/empty-dir.sh';
-	ln -f 'scripts/bash/empty-dir.sh' $install_directory/empty-dir;
-	#chmod 777 'scripts/bash/exp_files.sh';
-	#ln -f 'scripts/bash/exp_files.sh' $install_directory/exp_files;
-	chmod 777 'scripts/bash/full-path.sh';
-	ln -f 'scripts/bash/full-path.sh' $install_directory/full-path;
-	chmod 777 'scripts/bash/gcc-predefined-macros.sh';
-	ln -f 'scripts/bash/gcc-predefined-macros.sh' $install_directory/gcc-predefined-macros;
-	chmod 777 'scripts/bash/gcc-search-paths.sh';
-	ln -f 'scripts/bash/gcc-search-paths.sh' $install_directory/gcc-search-paths;
-	#chmod 777 'scripts/bash/getrom.sh';
-	#ln -f 'scripts/bash/getrom.sh' $install_directory/getrom;
-	chmod 777 'scripts/bash/git-current.sh';
-	ln -f 'scripts/bash/git-current.sh' $install_directory/git-current;
-	chmod 777 'scripts/bash/git-lazy.sh';
-	ln -f 'scripts/bash/git-lazy.sh' $install_directory/git-lazy;
-	chmod 777 'scripts/bash/git-update.sh';
-	ln -f 'scripts/bash/git-update.sh' $install_directory/git-update;
-	chmod 777 'scripts/bash/gnome-screen-idle-delay.sh';
-	ln -f 'scripts/bash/gnome-screen-idle-delay.sh' $install_directory/gnome-screen-idle-delay;
-	chmod 777 'scripts/bash/gnome-screen-idle-delay-10.sh';
-	ln -f 'scripts/bash/gnome-screen-idle-delay-10.sh' $install_directory/gnome-screen-idle-delay-10;
-	chmod 777 'scripts/bash/gnome-screen-idle-delay-600.sh';
-	ln -f 'scripts/bash/gnome-screen-idle-delay-600.sh' $install_directory/gnome-screen-idle-delay-600;
-	chmod 777 'scripts/bash/init-go-project.sh';
-	ln -f 'scripts/bash/init-go-project.sh' $install_directory/init-go-project;
-	chmod 777 'scripts/bash/mac-idle-time.sh';
-	ln -f 'scripts/bash/mac-idle-time.sh' $install_directory/mac-idle-time;
-	chmod 777 'scripts/bash/profilerc.sh';
-	ln -f 'scripts/bash/profilerc.sh' ~/.profilerc;
-	#chmod 777 'scripts/bash/searchrom.sh';
-	#ln -f 'scripts/bash/searchrom.sh' $install_directory/searchrom;
-	chmod 777 'scripts/bash/git-fatfiles.sh';
-	ln -f 'scripts/bash/git-fatfiles.sh' $install_directory/git-fatfiles;
-	chmod 777 'scripts/bash/git-listobjectsbysize.sh';
-	ln -f 'scripts/bash/git-listobjectsbysize.sh' $install_directory/git-listobjectsbysize;
+	['scripts/bash/clang-linker-info.sh']="$install_directory/clang-linker-info"
+	['scripts/bash/clang-predefined-macros.sh']="$install_directory/clang-predefined-macros"
+	['scripts/bash/clang-search-paths.sh']="$install_directory/clang-search-paths"
+	#['scripts/bash/clense-bad-roms.sh']="$install_directory/clense-bad-roms"
+	['scripts/bash/create-c-project-directories.sh']="$install_directory/create-c-project-directories"
+	#['scripts/bash/create-node-project-directories.sh']="$install_directory/create-node-project-directories"
+	#['scripts/bash/create-node-project-directory.sh']="$install_directory/create-node-project-directory"
+	['scripts/bash/empty-dir.sh']="$install_directory/empty-dir"
+	#['scripts/bash/exp_files.sh']="$install_directory/exp_files"
+	['scripts/bash/full-path.sh']="$install_directory/full-path"
+	['scripts/bash/gcc-predefined-macros.sh']="$install_directory/gcc-predefined-macros"
+	['scripts/bash/gcc-search-paths.sh']="$install_directory/gcc-search-paths"
+	#['scripts/bash/getrom.sh']="$install_directory/getrom"
+	['scripts/bash/git-current.sh']="$install_directory/git-current"
+	['scripts/bash/git-lazy.sh']="$install_directory/git-lazy"
+	['scripts/bash/git-update.sh']="$install_directory/git-update"
+	['scripts/bash/gnome-screen-idle-delay.sh']="$install_directory/gnome-screen-idle-delay"
+	['scripts/bash/gnome-screen-idle-delay-10.sh']="$install_directory/gnome-screen-idle-delay-10"
+	['scripts/bash/gnome-screen-idle-delay-600.sh']="$install_directory/gnome-screen-idle-delay-600"
+	['scripts/bash/init-go-project.sh']="$install_directory/init-go-project"
+	['scripts/bash/mac-idle-time.sh']="$install_directory/mac-idle-time"
+	['scripts/bash/profilerc.sh']="~/.profilerc"
+	#['scripts/bash/searchrom.sh']="$install_directory/searchrom"
+	['scripts/bash/git-fatfiles.sh']="$install_directory/git-fatfiles"
+	['scripts/bash/git-listobjectsbysize.sh']="$install_directory/git-listobjectsbysize"
 	#scripts/vim
-	chmod 777 'scripts/vim/expansions.vim';
-	ln -f 'scripts/vim/expansions.vim' ~/.expansions;
-	chmod 777 'scripts/vim/external_expand.vim';
-	ln -f 'scripts/vim/external_expand.vim' ~/.external_expand;
+	['scripts/vim/expansions.vim']="~/.expansions"
+	['scripts/vim/external_expand.vim']="~/.external_expand"
 	#scripts/node
-	chmod 777 'scripts/node/create-github-project.js';
-	ln -f 'scripts/node/create-github-project.js' $install_directory/create-github-project;
-	chmod 777 'scripts/node/init-node-project.js';
-	ln -f 'scripts/node/init-node-project.js' $install_directory/init-node-project;
+	['scripts/node/create-github-project.js']="$install_directory/create-github-project"
+	['scripts/node/init-node-project.js']="$install_directory/init-node-project"
+);
+install_command='echo';
+chmod_command='chmod 777';
+if [[ $mode != 1 ]]; then
+	map_keys_string=${!Map[@]};
+	echo $map_keys_string;
+	#declare -a map_keys_array=( $map_keys_string ); #working
+	#echo ${map_keys_array[0]};
+	for key_string in "${!Map[@]}"; do
+		#echo $key_string;
+		$chmod_command $key_string;
+		$install_command "$key_string" "${Map[$key_string]}";
+	done
 else
 	#scripts/bash
 	rm -f $install_directory/clang-linker-info;
