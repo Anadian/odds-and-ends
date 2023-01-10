9a056fccc757ce1f15e4eba49c8080a366f08458
Anadian <willand@yandex.com>
2023-01-10 13:37:04 -0500

    Streamlined profilerc.

--- scripts/bash/profilerc.sh
+++ scripts/bash/profilerc.sh
@@ -6,8 +6,9 @@ export GOPATH=$HOME/dev/go
 export GITHUB_USERNAME='Anadian';
 #printf 'PATH: %s\n' $PATH;
 export HOMEBIN=$HOME/.local/bin;
-export PATH=$HOMEBIN:$HOME/bin:/snap/bin:$PATH:$GOPATH/bin;
-export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules:$HOME/.local/lib/node_modules;
+export PNPM_HOME="$HOME/.local/share/pnpm";
+export NODE_PATH="$PNPM_HOME/global/5/node_modules:$NODE_PATH:/usr/local/lib/node_modules:$HOME/.local/lib/node_modules";
+export PATH=$HOMEBIN:$HOME/bin:$PNPM_HOME:/snap/bin:$PATH:$GOPATH/bin;
 if [[ $(uname -o) == 'Android' ]]; then
 	export NODE_PATH=/data/data/com.termux/files/usr/lib/node_modules:$NODE_PATH;
 else
